56f5f5cb44f8ba8ccc2b89d2d873ef5368cd5a64
Romario Mendes <romariomendesgoes@gmail.com>
2021-03-20 10:33:36 -0500

    desafio-02-componentizando-a-aplicacao

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setdGenreId: Function;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  setdGenreId,
+}: SideBarProps) {
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+        <div className="buttons-container">
+          {genres.map((genre) => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => setdGenreId(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
